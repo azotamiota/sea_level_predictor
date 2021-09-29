55d57fdc87844dd0f6b1f66b02f0322caa423a2e
azotamiota <87644571+azotamiota@users.noreply.github.com>
2021-09-29 16:17:54 +0300

    Update sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -31,7 +31,3 @@ def draw_plot():
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
-
-draw_plot()
-plt.show()
-print(plt.rcParams.keys())
